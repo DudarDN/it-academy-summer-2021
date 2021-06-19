8fb99378a9584b9d3e9f0073c1c9294b3929335d
DudarDN <Dudar91@gmail.com>
2021-06-19 17:52:37 +0300

    Написал программу FizzBuzz
    Программа выводит список чисел от 1 до 100, предварительно проверив, делится ли каждое из этих чисел на 3 и (или) 5.

---
+++ src/homework3/task1.py
@@ -0,0 +1,22 @@
+# Напишите программу, которая печатает цифры от 1 до 100,
+# но вместо чисел, кратных 3 пишет Fizz, вместо чисел кратный 5 пишет Buzz,
+# а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz.
+
+
+list_num = list(range(1, 101))
+list_FizzBuzz = []
+for i in list_num:
+    if i % 3 == 0:
+        if i % 5 == 0:
+            i = "FizzBuzz"
+            list_FizzBuzz.append(i)
+        else:
+            i = "Fizz"
+            list_FizzBuzz.append(i)
+    elif i % 5 == 0:
+        i = "Buzz"
+        list_FizzBuzz.append(i)
+    else:
+        list_FizzBuzz.append(i)
+
+print(list_FizzBuzz)
