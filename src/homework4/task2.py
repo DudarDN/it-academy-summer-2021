1086c729c6f92e7b0c8e88e091bfb6b55a0a8db6
DudarDN <Dudar91@gmail.com>
2021-06-27 15:44:05 +0300

    Отредактиравал стиль кода

--- src/homework4/task2.py
+++ src/homework4/task2.py
@@ -6,13 +6,15 @@
 # затем идут названия городов этой страны.
 # В следующей строке записано число M,
 # далее идут M запросов — названия каких-то M городов, перечисленных выше.
-# Для каждого из запроса выведите название страны, в котором находится данный город.
+# Для каждого из запроса выведите название страны,
+# в котором находится данный город.
 
 n_countries = int(input("Введите кол-во стран: "))
 dict_country_and_cities = {}
 for i in range(n_countries):
     str_country_and_cities = (input("Введите сначала страну, "
-                                    "а затем города, находящиеся в этой стране: "))
+                                    "а затем города,"
+                                    " находящиеся в этой стране: "))
     list_country_and_cities = str_country_and_cities.split()
     for city in list_country_and_cities[1:]:
         dict_country_and_cities[city] = list_country_and_cities[0]
--- src/homework4/task7.py
+++ src/homework4/task7.py
@@ -2,12 +2,14 @@
 # Вычислите их наибольший общий делитель
 # при помощи алгоритма Евклида (мы не знаем функции и рекурсию).
 
-number_1, number_2 = int(input("Введите натуральное число: ")),\
-                     int(input("Введите натуральное число: "))
-a, b = number_1, number_2
-while number_1 != number_2:
-    if number_1 > number_2:
-        number_1 -= number_2
+num_1 = int(input("Введите натуральное число: "))
+num_2 = int(input("Введите натуральное число: "))
+a, b = num_1, num_2
+while num_1 != num_2:
+    if num_1 > num_2:
+        num_1 -= num_2
     else:
-        number_2 -= number_1
-print('Наибольший общий делитель чисел', a, "и", b, "равен " + str(number_1) + ".")
+        num_2 -= num_1
+print(
+    "Наибольший общий делитель чисел", a, "и", b, "равен " + str(num_1) + "."
+)
