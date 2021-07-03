7694166eb79132717c4e623763c85d6414a1cae5
DudarDN <Dudar91@gmail.com>
2021-07-04 01:23:41 +0300

    Изменил структуру словаря в задаче о городах и странах

--- src/homework4/task2.py
+++ src/homework4/task2.py
@@ -14,15 +14,19 @@
 n_countries = int(input("Введите кол-во стран: "))
 dict_country_and_cities = {}
 for i in range(n_countries):
+    list_cities = []
     str_country_and_cities = (input("Введите сначала страну, "
                                     "а затем города,"
                                     " находящиеся в этой стране: "))
     list_country_and_cities = str_country_and_cities.split()
-    for city in list_country_and_cities[1:]:
-        dict_country_and_cities[city] = list_country_and_cities[0]
+    for cities_of_thr_country in list_country_and_cities[1:]:
+        list_cities.append(cities_of_thr_country)
+        dict_country_and_cities[list_country_and_cities[0]] = list_cities
 cities = int(input("Введите кол-во запросов на поиск страны по городу: "))
 countries = ""
 for i in range(cities):
     city_search = input("Введите название города: ")
-    countries += dict_country_and_cities[city_search] + "\n"
+    for country, cities_of_the_country in dict_country_and_cities.items():
+        if city_search in cities_of_the_country:
+            countries += country + "\n"
 print(countries)
