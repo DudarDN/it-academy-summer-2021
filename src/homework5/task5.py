3652477f1e67edf950c13dcfa5e0bdc274a4343f
DudarDN <Dudar91@gmail.com>
2021-07-17 19:25:01 +0300

    Переписал задачи со степенями двойки в функции

--- src/homework5/task5.py
+++ src/homework5/task5.py
@@ -1,16 +1,21 @@
 """Написать программу которая находит ближайшую степень двойки
 к введенному числу. 10(8), 20(16), 1(1), 13(16)
-
 """
 
-number = int(input("Введите число: "))
-two_power_big = 2
-power = 1
-while two_power_big < number:
-    power += 1
-    two_power_big = 2 ** power
-two_power_small = 2 ** (power - 1)
-if two_power_big - number <= number - two_power_small:
-    print("Ближайшее число, являющееся степенью двойки: ", two_power_big)
-else:
-    print("Ближайшее число, являющееся степенью двойки: ", two_power_small)
+
+def power_of_two(number):
+    two_power_big = 2
+    power = 1
+    while two_power_big < number:
+        power += 1
+        two_power_big = 2 ** power
+    two_power_small = 2 ** (power - 1)
+    if two_power_big - number <= number - two_power_small:
+        print("Ближайшее число, являющееся степенью двойки: ", two_power_big)
+    else:
+        print("Ближайшее число, являющееся степенью двойки: ", two_power_small)
+
+
+power_of_two(8)
+power_of_two(2)
+power_of_two(1)
--- src/homework5/task6.py
+++ src/homework5/task6.py
@@ -1,15 +1,20 @@
 """Вводится число, найти его максимальный делитель,
  являющийся степенью двойки. 10(2) 16(16), 12(4).
-
 """
 
-number = int(input("Введите число: "))
-divisor = int()
-power = 0
-while (2 ** power) <= number:
-    if number % (2 ** power) == 0:
-        divisor = (2 ** power)
-        power += 1
-    else:
-        break
-print("Максимальный делитель, являющийся степенью двойки: ", divisor)
+
+def max_divisor(number):
+    divisor = int()
+    power = 0
+    while (2 ** power) <= number:
+        if number % (2 ** power) == 0:
+            divisor = (2 ** power)
+            power += 1
+        else:
+            break
+    print("Максимальный делитель, являющийся степенью двойки: ", divisor)
+
+
+max_divisor(10)
+max_divisor(12)
+max_divisor(11)
