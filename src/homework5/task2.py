8e72bf53081a2e19b93f93d1cfd405a8d8ca20c0
DudarDN <Dudar91@gmail.com>
2021-07-17 22:58:54 +0300

    Переделал задание с декоратором

--- src/homework5/task2.py
+++ src/homework5/task2.py
@@ -1,16 +1,14 @@
 """ Создайте декоратор, который хранит результаты вызовов функции
 (за все время вызовов, не только текущий запуск программы).
-
 """
 
 
 def decorator(func_new):
-    results = []
 
     def wrapper(*args, **kwargs):
-        nonlocal results
-        func_new(*args, **kwargs)
-        results.append(func_new(*args, **kwargs))
+        results = func_new(*args, **kwargs)
+        with open("results.txt", "a") as res:
+            res.write(str(results) + "\n")
         return results
 
     return wrapper
@@ -21,14 +19,14 @@ def func_addition(x, y):
     return x + y
 
 
-print(func_addition(2, 3))
+print(func_addition("s", "f"))
 print(func_addition(3, 5))
 print(func_addition(5, 5))
 
 
 @decorator
 def func_multiplication(x, y):
-    return x * y
+    return x - y
 
 
 print(func_multiplication(2, 3))
