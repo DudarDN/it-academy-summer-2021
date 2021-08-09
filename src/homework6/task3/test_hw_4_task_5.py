a173eac368010540585a2dbdac51b0af3c53fe5b
DudarDN <Dudar91@gmail.com>
2021-08-09 14:43:55 +0300

    task3: Добавил строки документации в тест. Дополнил негативные тесты

--- src/homework6/task3/test_hw_4_task_5.py
+++ src/homework6/task3/test_hw_4_task_5.py
@@ -4,7 +4,14 @@ import unittest
 
 class Test(unittest.TestCase):
 
-    def test_positive_1(self):
+    def test_positive(self):
+        """Проверка корректной работы функции с различными входными данными
+
+        Тестирование способности функции работать со значениями по умалчанию,
+        с аргументами - различными типами данных,
+        с пустыми коллекциями.
+
+        """
         result = hw_4_task_5.all_languages()
         self.assertEqual(result[0], 1)
         self.assertCountEqual(result[1], ["Belarusian"])
@@ -34,13 +41,31 @@ class Test(unittest.TestCase):
         self.assertEqual(result, (0, [], 0, []))
 
     def test_negative(self):
+        """Проверка некоректной работы функции с различными входными данными
+
+        Передача аргумента в виде строки приведет к ошибочному результату.
+        Передача чисел и славаря, коллекций с числами,
+        а также использование позиционных аргументов вернет ошибку.
+
+        """
         result = hw_4_task_5.all_languages(student_1=["Belarusian", "Russian",
                                                       "Polish", "English"],
                                            student_2=("German",),
                                            student_3="France")
         self.assertNotEqual(result[2], 6)
         with self.assertRaises(TypeError):
-            hw_4_task_5.all_languages(student_1=23)
+            hw_4_task_5.all_languages(student_1=23, student_2=23.34)
+            hw_4_task_5.all_languages(student_1=[23],
+                                      student_2=(23.34, 23),
+                                      student_3={23.34, 23}
+                                      )
+            hw_4_task_5.all_languages(student_1={"student_1": ["Russian",
+                                                               "English",
+                                                               "Belarusian"]})
+            hw_4_task_5.all_languages(["Belarusian", "Russian"],
+                                      ["Polish", "English", "German"])
+            hw_4_task_5.all_languages({"student_1": ["Russian", "English",
+                                                     "Belarusian"]})
 
 
 if __name__ == '__main__':
