64a7b46935c48be5087c4c1a8238a02ed47385ff
DudarDN <Dudar91@gmail.com>
2021-08-16 02:33:04 +0300

    Разделил тестовые случаи и оформил их отдельными тестами

--- src/homework6/task3/test_hw_4_task_5.py
+++ src/homework6/task3/test_hw_4_task_5.py
@@ -4,14 +4,8 @@ import unittest
 
 class Test(unittest.TestCase):
 
-    def test_positive(self):
-        """Проверка корректной работы функции с различными входными данными.
-
-        Тестирование способности функции работать со значениями по умолчанию,
-        с аргументами - различными типами данных,
-        с пустыми коллекциями.
-
-        """
+    def test_positive_default_values(self):
+        """Тестирование работы функции со значениями по умолчанию"""
         result = hw_4_task_5.all_languages()
         self.assertEqual(result[0], 1)
         self.assertCountEqual(result[1], ["Belarusian"])
@@ -19,6 +13,17 @@ class Test(unittest.TestCase):
         self.assertCountEqual(result[3],
                               ["Belarusian", "Russian", "Polish", "English",
                                "German"])
+        result = hw_4_task_5.all_languages(student_1=["Belarusian", "Russian",
+                                                      "Polish", "English"])
+        self.assertEqual(result[0], 4)
+        self.assertCountEqual(result[1], ["Belarusian", "Russian",
+                                          "Polish", "English"])
+        self.assertEqual(result[2], 4)
+        self.assertCountEqual(result[3], ["Belarusian", "Russian",
+                                          "Polish", "English"])
+
+    def test_positive_data_types(self):
+        """Тестирование работы функции с различными типами данных"""
         result = hw_4_task_5.all_languages(student_1=["Belarusian", "Russian",
                                                       "Polish", "English"],
                                            student_2=("German",),
@@ -28,31 +33,23 @@ class Test(unittest.TestCase):
         self.assertEqual(result[2], 6)
         self.assertCountEqual(result[3], ["Belarusian", "Russian", "Polish",
                                           "English", "German", "France"])
-        result = hw_4_task_5.all_languages(student_1=["Belarusian", "Russian",
-                                                      "Polish", "English"])
-        self.assertEqual(result[0], 4)
-        self.assertCountEqual(result[1], ["Belarusian", "Russian",
-                                          "Polish", "English"])
-        self.assertEqual(result[2], 4)
-        self.assertCountEqual(result[3], ["Belarusian", "Russian",
-                                          "Polish", "English"])
+
+    def test_positive_empty_collections(self):
         result = hw_4_task_5.all_languages(student_1=[], student_2={},
                                            student3=())
         self.assertEqual(result, (0, [], 0, []))
 
-    def test_negative(self):
-        """Проверка некоректной работы функции с различными входными данными.
+    def test_negative_string(self):
+        """Передача аргумента в виде строки приведет к неверному результату"""
 
-        Передача аргумента в виде строки приведет к неверному результату.
-        Передача чисел и славаря, коллекций с числами,
-        а также использование позиционных аргументов вернет ошибку.
-
-        """
         result = hw_4_task_5.all_languages(student_1=["Belarusian", "Russian",
                                                       "Polish", "English"],
                                            student_2=("German",),
                                            student_3="France")
         self.assertNotEqual(result[2], 6)
+
+    def test_negative_other_data_types(self):
+        """Передача неккорректных типов данных вернет ошибку"""
         with self.assertRaises(TypeError):
             hw_4_task_5.all_languages(student_1=23, student_2=23.34)
             hw_4_task_5.all_languages(student_1=[23],
@@ -62,6 +59,10 @@ class Test(unittest.TestCase):
             hw_4_task_5.all_languages(student_1={"student_1": ["Russian",
                                                                "English",
                                                                "Belarusian"]})
+
+    def test_negative_positional_arguments(self):
+        """Передача позиционных аргументов вернет ошибку"""
+        with self.assertRaises(TypeError):
             hw_4_task_5.all_languages(["Belarusian", "Russian"],
                                       ["Polish", "English", "German"])
             hw_4_task_5.all_languages({"student_1": ["Russian", "English",
